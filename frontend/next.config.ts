e4cd302f94bc84ae94630b0880e40032230c8451
Edoardo Conti <edoandcode@gmail.com>
2025-12-22 17:18:54 +0100

    frontend - added standalone output in next.config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isDev: boolean = process.env.NODE_ENV === 'development';
+
+
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
+  compiler: {
+    removeConsole: !isDev,
+  },
 };
 
 export default nextConfig;
